2c76122d9cc76f08c3cf3ae2a66bc739fc99804b
adam <hanfengyizuflj@qq.com>
2021-02-22 21:08:45 +0800

    766

---
+++ 766_isToeplitzMatrix/Main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	test1()
+	test2()
+}
+func test2() {
+	matrix := [][]int{
+		{41, 45},
+		{81, 41},
+		{73, 81},
+		{47, 73},
+		{0, 47},
+		{79, 76}}
+	fmt.Println(isToeplitzMatrix(matrix))
+}
+func test1() {
+	matrix := [][]int{
+		{1, 2, 3, 4},
+		{5, 1, 2, 3},
+		{9, 5, 1, 2}}
+	fmt.Println(isToeplitzMatrix(matrix))
+}
+
+func isToeplitzMatrix(matrix [][]int) bool {
+	M := len(matrix)
+	N := len(matrix[0])
+	m, n := 0, 0
+	start := 0
+	for i := 0; i < N; i++ {
+		start = matrix[0][i]
+		m, n = 1, i+1
+		for m < M && n < N {
+			if matrix[m][n]^start != 0 {
+				return false
+			}
+			m++
+			n++
+		}
+	}
+
+	for j := 1; j < M; j++ {
+		start := matrix[j][0]
+		m, n := j+1, 0+1
+		for m < M && n < N {
+			if matrix[m][n]^start != 0 {
+				return false
+			}
+			m++
+			n++
+		}
+	}
+	return true
+}
